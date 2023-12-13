307d3098dcac3aa184df649ef4ffc16b5e1d41f6
Haider11201 <ghulamhaider11201@gmail.com>
2023-12-14 02:58:47 +0500

    weather App

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+window.weatherChacking = function () {
+    let cityName = document.querySelector("#formGroupExampleInput").value;
+
+    let API_Key = '9cbee56634f1427788f8b341807611fa';
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_Key}&units=metric`)
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    document.querySelector("#cityAndMonth").innerHTML = `${response.data.name} ${response.data.sys.country}`
+    document.querySelector("#temperatur").innerHTML = `${response.data.main.temp}°C`
+    document.querySelector("#min_tem").innerHTML = `Min temp : <strong>${response.data.main.temp_min}°C</strong>`
+    document.querySelector("#max_tem").innerHTML = `Max temp : <strong>${response.data.main.temp_max}°C</strong>`
+    document.querySelector("#weatherSetuation").innerHTML = `${response.data.weather[0].main}`
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error.data);
+    document.querySelector("#weatherSetuation").innerHTML = " error in getting weather data"
+  });
+  
+}
